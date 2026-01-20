648ccbbb676cdf7d19389ea04608e3a223d11ce1
chgasche <christoph.gasche@student.oulu.fi>
2026-01-20 22:38:47 +0200

    Homework 1

--- Homework1/app/src/main/java/com/example/homework1application/MainActivity.kt
+++ Homework1/app/src/main/java/com/example/homework1application/MainActivity.kt
@@ -3,18 +3,40 @@ package com.example.homework1application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.homework1application.sampledata.SampleData
 import com.example.homework1application.ui.theme.Homework1ApplicationTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    /*override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
@@ -27,9 +49,163 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }*/
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Homework1ApplicationTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    /*FlightCard(Flight("19:30", "AY560", "HEL", "scheduled", "C2", "A2", "20:35"))*/
+                    FlightList(SampleData.flightlistSample)
+                }
+            }
+        }
     }
 }
 
+data class Flight(val time: String, val flightnr: String, val dest: String, val status: String, val checkin: String, val gate: String, val timeSTA: String)
+
+@Composable
+fun FlightCard(flight: Flight) {
+
+    // Set airline logo
+    val airlineLogo = if(flight.flightnr.contains("AY")) {
+        R.drawable.ay
+    } else if(flight.flightnr.contains("SK")) {
+        R.drawable.sk
+    } else {
+        R.drawable.default_airline
+    }
+
+    // We keep track if the message is expanded or not in this
+    var isExpanded by remember { mutableStateOf(false) }
+
+    // Proceed
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(airlineLogo),
+            contentDescription = "Airline Logo",
+            modifier = Modifier
+                // Set image size
+                .size(30.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+                // Set border
+                .border(1.2.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(6.dp))
+        Surface(
+            shape = MaterialTheme.shapes.extraSmall,
+            shadowElevation = 1.dp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .animateContentSize().padding(1.dp)
+        ) {
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+                Row(modifier = Modifier.padding(horizontal = 8.dp, vertical = 2.dp)) {
+                    Text(
+                        text = flight.time,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(text = flight.flightnr)
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(
+                        text = flight.dest,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(
+                        text = flight.status.uppercase(),
+                        color = MaterialTheme.colorScheme.tertiary
+                    )
+                }
+                if (isExpanded) {
+                    Row(modifier = Modifier.padding(all = 4.dp)) {
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Text(
+                            text = "OUL",
+                            fontWeight = FontWeight.Bold
+                        )
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Text(
+                            text = flight.time,
+                            color = MaterialTheme.colorScheme.secondary
+                        )
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Text(text = " ✈ ")
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Text(
+                            text = flight.timeSTA,
+                            color = MaterialTheme.colorScheme.secondary
+                        )
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Text(
+                            text = flight.dest,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                    Row(modifier = Modifier.padding(all = 4.dp)) {
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Text(
+                            text = "Gate",
+                            fontWeight = FontWeight.Bold
+                        )
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Text(
+                            text = flight.gate,
+                            color = MaterialTheme.colorScheme.secondary
+                        )
+                    }
+                    Row(modifier = Modifier.padding(all = 4.dp)) {
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Text(
+                            text = "Check-In",
+                            fontWeight = FontWeight.Bold
+                        )
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Text(
+                            text = flight.checkin,
+                            color = MaterialTheme.colorScheme.secondary
+                        )
+                    }
+                }
+            }
+    }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewFlightCard() {
+    Homework1ApplicationTheme {
+        Surface {
+            FlightCard(
+                flight = Flight("19:30", "AY560", "HEL", "scheduled", "C12", "A2", "20:35")
+            )
+        }
+    }
+}
+
+
+@Composable
+fun FlightList(flights: List<Flight>) {
+    LazyColumn {
+        items(flights) { flight ->
+            FlightCard(flight)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewFlightList() {
+    Homework1ApplicationTheme {
+        FlightList(SampleData.flightlistSample)
+    }
+}
+
+/*
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -44,4 +220,4 @@ fun GreetingPreview() {
     Homework1ApplicationTheme {
         Greeting("Android")
     }
-}
+}*/
---
+++ Homework1/app/src/main/java/com/example/homework1application/sampledata/SampleData.kt
@@ -0,0 +1,38 @@
+package com.example.homework1application.sampledata
+
+import com.example.homework1application.Flight
+
+/**
+ * SampleData for Jetpack Compose Tutorial
+ */
+object SampleData {
+    // Sample data
+    val flightlistSample = listOf(
+        Flight("06:15", "AY101", "HEL", "departed", "C1", "A1", "07:15"),
+        Flight("06:45", "SK202", "HEL", "departed", "C3", "A3", "07:45"),
+        Flight("07:10", "AY305", "RVN", "departed", "C2", "A2", "08:50"),
+        Flight("07:30", "SK410", "KTT", "departed", "C3", "A3", "09:15"),
+        Flight("08:00", "AY215", "HEL", "departed", "C2", "A2", "09:00"),
+        Flight("08:30", "SK322", "HEL", "departed", "C3", "A3", "09:30"),
+        Flight("09:05", "AY450", "LHR", "departed", "C1", "A1", "13:05"),
+        Flight("09:30", "SK510", "SFO", "departed", "C3", "E1", "11:30"),
+        Flight("10:00", "AY180", "HEL", "departed", "C2", "A2", "11:00"),
+        Flight("10:25", "SK270", "HEL", "departed", "C3", "A3", "11:25"),
+        Flight("11:00", "AY220", "RVN", "departed", "C2", "A2", "12:40"),
+        Flight("11:40", "SK330", "KTT", "departed", "C3", "A3", "13:25"),
+        Flight("12:15", "AY505", "HEL", "on time", "C1", "A1", "13:15"),
+        Flight("12:50", "SK600", "HEL", "on time", "C3", "A3", "13:50"),
+        Flight("13:30", "AY325", "LHR", "on time", "C2", "E1", "17:30"),
+        Flight("14:00", "SK410", "HEL", "on time", "C3", "A2", "01:00"),
+        Flight("14:45", "AY450", "HEL", "scheduled", "C1", "A1", "15:45"),
+        Flight("15:15", "SK520", "HEL", "scheduled", "C3", "A3", "16:15"),
+        Flight("16:00", "AY470", "IVL", "scheduled", "C2", "A2", "18:05"),
+        Flight("16:30", "SK530", "KTT", "scheduled", "C3", "A3", "18:15"),
+        Flight("17:10", "AY490", "HEL", "scheduled", "C1", "A1", "18:10"),
+        Flight("17:45", "SK540", "HEL", "scheduled", "C3", "A3", "18:45"),
+        Flight("19:30", "AY560", "HEL", "scheduled", "C1", "A1", "20:30"),
+        Flight("20:30", "AY562", "HEL", "scheduled", "C2", "A2", "21:30"),
+        Flight("21:30", "SK2314", "KTT", "scheduled", "C3", "A3", "23:15"),
+        Flight("22:30", "AY566", "HEL", "scheduled", "C1", "A1", "23:30")
+    )
+}
